b8fd9b335f36bfb787fd228f754dbdff55a22842
edinelsonslima <edinelsonlima86@gmail.com>
2021-10-11 21:36:44 -0300

    readme

--- views/about/sobre.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-    setInterval(() => {
-      const load = document.querySelector(".loading");
-      load.style.display = "none";
-    }, 300);
-  };
-  
--- views/skill/skill.js
+++
@@ -1,8 +0,0 @@
-//tela de login
-window.onload = () => {
-    setInterval(() => {
-      const load = document.querySelector(".loading");
-      load.style.display = "none";
-    }, 300);
-  };
-  
